678b9f3df2187fcd20891466761e2e2987083519
Kiavash <Kiavashtaherian@gmail.com>
2023-09-24 21:22:25 +0330

    Mainloop

--- Snake Game Codes/Main-Program.py
+++ Snake Game Codes/Main-Program.py
@@ -1,5 +1,8 @@
+from Snake import Snake
+from Food import Food
 from tkinter import *
-import Snake
+
+
 window = Tk()
 
 
@@ -7,5 +10,7 @@ window.title("Snake Game")
 window.resizable(False, False)
 
 
-
+window.update()
 snake = Snake()
+food = Food()
+window.mainloop()
