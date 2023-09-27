e0fab225a3835fc9770ab9f8b37a50b1dd8b9457
Kiavash <Kiavashtaherian@gmail.com>
2023-09-27 17:10:39 +0330

    change_direction added

--- Snake Game Codes/Functions.py
+++ Snake Game Codes/Functions.py
@@ -12,3 +12,20 @@ def next_turn(snake, food):
    snake.coordinates.insert(0, [x, y])
    square = canvas.create_rectangle(x, y , x + SPACE_SIZE, y + SPACE_SIZE,fill =SNAKE_COLOR)
    snake.squares.insert(0, square)
+   
+def change_direction(new_dir):
+   global direction
+
+   if new_dir == "left":
+      if direction != "right":
+         
+         direction = new_dir
+   elif new_dir == "right":
+      if direction != "left":
+         direction = new_dir
+   elif new_dir == "up":
+      if direction != "down":
+         direction = new_dir
+   elif new_dir == "down":
+      if direction != "up":
+         direction = new_dir
