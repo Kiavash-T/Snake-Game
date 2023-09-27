9276285d289e255f080b6490143be1f4f0625058
Kiavash <Kiavashtaherian@gmail.com>
2023-09-27 17:41:19 +0330

    check_game_over created

--- Snake Game Codes/Functions.py
+++ Snake Game Codes/Functions.py
@@ -12,7 +12,7 @@ def next_turn(snake, food):
    snake.coordinates.insert(0, [x, y])
    square = canvas.create_rectangle(x, y , x + SPACE_SIZE, y + SPACE_SIZE,fill =SNAKE_COLOR)
    snake.squares.insert(0, square)
-   
+
 def change_direction(new_dir):
    global direction
 
@@ -29,3 +29,16 @@ def change_direction(new_dir):
    elif new_dir == "down":
       if direction != "up":
          direction = new_dir
+
+def check_game_over(snake):
+   x, y = snake.coordinates[0]
+    
+   if x < 0 or x > GAME_WIDTH - SPACE_SIZE:
+      return True
+   if y < 0 or y > GAME_HEIGHT- SPACE_SIZE :
+      return True
+
+   for sq in snake.coordinates[1:]:
+      if x == sq[0] and y == sq[1]:
+         return True
+   return False
